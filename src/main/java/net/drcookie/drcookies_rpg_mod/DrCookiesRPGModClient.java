7d73d655ab208a013cfa36170c895bd5b57534e5
DoktorCookie <erik_ee@t-online.de>
2023-12-20 15:09:44 +0100

    Initial commit

---
+++ src/main/java/net/drcookie/drcookies_rpg_mod/DrCookiesRPGModClient.java
@@ -0,0 +1,10 @@
+package net.drcookie.drcookies_rpg_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DrCookiesRPGModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
