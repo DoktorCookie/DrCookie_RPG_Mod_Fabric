062f3e5f95d0aa83c3e7f869a06ae657adf66504
DoktorCookie <erik_ee@t-online.de>
2023-12-23 12:20:40 +0100

    Added Blocks creative tab

--- src/main/java/net/drcookie/drcookies_rpg_mod/item/ModItemGroups.java
+++ src/main/java/net/drcookie/drcookies_rpg_mod/item/ModItemGroups.java
@@ -20,6 +20,17 @@ public class ModItemGroups {
                         entries.add(ModItems.SWAMP_IRON_RAW);
                         entries.add(ModItems.SWAMP_IRON_INGOT);
 
+
+                    }).build()
+
+    );
+
+    public static final ItemGroup DRCOOKIE_RPG_BLOCKS_GROUP = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(DrCookiesRPGMod.MOD_ID, "rpg_blocks"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.rpg_blocks"))
+                    .icon( () -> new ItemStack(ModBlocks.SWAMP_IRON_ORE)).entries((displayContext, entries) -> {
+
+
                         entries.add(ModBlocks.SWAMP_IRON_RAW_BLOCK);
                         entries.add(ModBlocks.SWAMP_IRON_ORE);
 
