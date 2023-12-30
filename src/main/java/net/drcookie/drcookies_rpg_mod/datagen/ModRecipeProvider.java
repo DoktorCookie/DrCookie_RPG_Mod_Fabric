2d26f3156fa9cb72b0f5190733c7dc9de1697ff2
DoktorCookie <erik_ee@t-online.de>
2023-12-30 20:58:36 +0100

    Added Swamp Iron Nugget Recipes Added groups to all generated recipes Added custom id's to all generated recipes, if necessary

--- src/main/java/net/drcookie/drcookies_rpg_mod/datagen/ModRecipeProvider.java
+++ src/main/java/net/drcookie/drcookies_rpg_mod/datagen/ModRecipeProvider.java
@@ -6,6 +6,7 @@ import net.drcookie.drcookies_rpg_mod.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.RecipeProvider;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.Items;
@@ -40,14 +41,34 @@ public class ModRecipeProvider  extends FabricRecipeProvider {
                 0.7f, 100, "swamp_iron_ingot");
 
 
-        //Swamp iron ingots to block and vis versa
-        offerReversibleCompactingRecipes(exporter, RecipeCategory.DECORATIONS, ModItems.SWAMP_IRON_INGOT,
-                RecipeCategory.BUILDING_BLOCKS, ModBlocks.SWAMP_IRON_BLOCK);
+        //Swamp iron ingots to block and vis versa.
+        //This way the compacting recipe ids can be changed, the group parameter makes "alternative recipes" function
+        //in the recipe book, just like the ones for gold and iron ingots.
+        //changing the recipe ids is better, the DataGen sometimes tries to create recipes with the same id if the results is the same
+        offerReversibleCompactingRecipes(exporter,
+                RecipeCategory.DECORATIONS, ModItems.SWAMP_IRON_INGOT,
+                RecipeCategory.BUILDING_BLOCKS, ModBlocks.SWAMP_IRON_BLOCK,
+                "swamp_iron_ingots_to_swamp_iron_block", "swamp_iron_block",
+                "swamp_iron_block_to_swamp_iron_ingots","swamp_iron_ingot");
 
 
         //Raw swamp iron to block and vis versa
-        offerReversibleCompactingRecipes(exporter, RecipeCategory.DECORATIONS, ModItems.SWAMP_IRON_RAW,
-                RecipeCategory.BUILDING_BLOCKS, ModBlocks.SWAMP_IRON_RAW_BLOCK);
+        offerReversibleCompactingRecipes(exporter,
+                RecipeCategory.DECORATIONS, ModItems.SWAMP_IRON_RAW,
+                RecipeCategory.BUILDING_BLOCKS, ModBlocks.SWAMP_IRON_RAW_BLOCK,
+                "swamp_iron_raw_to_swamp_item_raw_block","swamp_iron_raw_block",
+                "swamp_iron_raw_block_to_swamp_iron_raw","swamp_iron_raw");
+
+
+        //Swamp Iron Nuggets to Ingots and vis versa
+
+        offerReversibleCompactingRecipes(exporter,
+                RecipeCategory.DECORATIONS, ModItems.SWAMP_IRON_NUGGET,
+                RecipeCategory.DECORATIONS, ModItems.SWAMP_IRON_INGOT,
+                "swamp_iron_nuggets_to_swamp_iron_ingot", "swamp_iron_ingot",
+                "swamp_iron_ingot_to_swamp_iron_nuggets", "swamp_iron_nugget");
+
+
 
 
         //Complete Custom Recipe (Category, Output, Count).patterns.inputs.criterion.offerTo
@@ -61,8 +82,10 @@ public class ModRecipeProvider  extends FabricRecipeProvider {
                 .input('I', ModItems.SWAMP_IRON_INGOT)
                 .criterion(hasItem(Items.STONE), conditionsFromItem(Items.STONE))
                 .criterion(hasItem(ModItems.SWAMP_IRON_INGOT), conditionsFromItem(ModItems.SWAMP_IRON_INGOT))
+                .group("swamp_iron_raw")
                 //.offerTo(exporter, new Identifier(getRecipeName(ModItems.SWAMP_IRON_RAW)));
-                .offerTo(exporter, new Identifier("test_craft_swamp_iron_raw"));
+                .offerTo(exporter, new Identifier("test_craft_swamp_iron_raw"))
+                ;
 
     }
 }
